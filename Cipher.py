ac002cba59337b06f635978c67402969047a8dea
Aman Kushwaha <70597017+Amank-root@users.noreply.github.com>
2022-01-05 12:59:27 +0530

    Create Cipher.py

---
+++ Cipher.py
@@ -0,0 +1,104 @@
+print('''
+    #Welcome To The Encryption/Decryption Program.
+    #This Version Maynot Include Custom Encryption
+''')
+
+print('''
+    # Process
+
+    # ==> Type "1" to encrypt a message
+    # ==> Type "2" to decrypt a message
+''')
+
+                        # Casear Cipher Encryptor (LoadUp Process)
+
+def encrypt(text, s):
+    result = ""
+ # transverse the plain text
+    for i in range(len(text)):
+        char = text[i]
+       # Encrypt uppercase characters in plain text
+
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+            
+    return result
+    # return result.replace('r', '_')  # try some other spacing technique.
+    # check the above function
+
+
+                        # Casear Cipher Decryptor (LoadUp Process)
+
+    
+def cipher_decrypt(ciphertext, key):
+
+    decrypted = ""
+
+    for c in ciphertext:
+
+        if c.isupper(): 
+
+            c_index = ord(c) - ord('A')
+
+            # shift the current character to left by key positions to get its original position
+            c_og_pos = (c_index - key) % 26 + ord('A')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.islower(): 
+
+            c_index = ord(c) - ord('a') 
+
+            c_og_pos = (c_index - key) % 26 + ord('a')
+
+            c_og = chr(c_og_pos)
+
+            decrypted += c_og
+
+        elif c.isdigit():
+
+            # if it's a number,shift its actual value 
+            c_og = (int(c) - key) % 10
+
+            decrypted += str(c_og)
+
+        else:
+
+            # if its neither alphabetical nor a number, just leave it like that
+            decrypted += c
+
+    return decrypted.replace("n", " ")
+    # return decrypted
+
+Get_The_Value = input("What Do You Want To Do: ")
+print(end="\n")
+
+if Get_The_Value == "1":
+    text_before_encrytion = input("Enter Text To Encrypt: ")
+    text_Transfered = text_before_encrytion
+    s = int(input("Enter Shift Value: "))
+
+    print ("Plain Text : " + text_Transfered)
+    print ("Shift Key : " + str(s))
+    print ("Cipher: " + encrypt(text_Transfered, s)) #Executing The Encryptor
+
+elif Get_The_Value == "2":
+    text_before_decrytion = input("Enter Text To Decrypt: ")
+    text_Transfered = text_before_decrytion
+    s = int(input("Enter Shift Value: "))
+
+    print ("Cipher Text : " + text_Transfered)
+    print ("Shift Key : " + str(s))
+    print ("Cipher: " + cipher_decrypt(text_Transfered, s)) #Executing The Decryptor
+else:
+    print("Invalid Input", "\n")
+    exit()
+
+
+
+                                                                            # The End
